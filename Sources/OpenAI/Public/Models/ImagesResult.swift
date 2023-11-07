68d01b02493f601d639c0b897ecacb3c45709e9f
Nathan Borror <nathan@nathanborror.com>
2023-11-06 21:44:32 -0800

    fixing image response shape

--- Sources/OpenAI/Public/Models/ImagesResult.swift
+++ Sources/OpenAI/Public/Models/ImagesResult.swift
@@ -9,21 +9,18 @@ import Foundation
 
 public struct ImagesResult: Codable, Equatable {
     
-    public struct URLResult: Codable, Equatable {
-        public let url: String
+    public struct ImageResult: Codable, Equatable {
+        public let url: String?
+        public let b64JSON: String?
+        public let revisedPrompt: String?
+        
+        enum CodingKeys: String, CodingKey {
+            case url
+            case b64JSON = "b64_json"
+            case revisedPrompt = "revised_prompt"
+        }
     }
     
     public let created: TimeInterval?
-    public let data: [URLResult]?
-    public let url: String?
-    public let b64JSON: String?
-    public let revisedPrompt: String?
-    
-    enum CodingKeys: String, CodingKey {
-        case created
-        case data
-        case url
-        case revisedPrompt = "revised_prompt"
-        case b64JSON = "b64_json"
-    }
+    public let data: [ImageResult]?
 }
