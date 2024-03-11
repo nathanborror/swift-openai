412e686ec6ad292289b410bca04a5dd438e7b9c9
Nathan Borror <nathan@nathanborror.com>
2024-03-10 21:02:00 -0700

    stiching together streaming responses

--- Sources/OpenAI/Private/StreamingSession.swift
+++ Sources/OpenAI/Private/StreamingSession.swift
@@ -14,6 +14,7 @@ final class StreamingSession<ResultType: Codable>: NSObject, Identifiable, URLSe
     var onProcessingError: ((StreamingSession, Error) -> Void)?
     var onComplete: ((StreamingSession, Error?) -> Void)?
     
+    private var streamingBuffer = ""
     private let streamingCompletionMarker = "[DONE]"
     private let urlRequest: URLRequest
     private lazy var urlSession: URLSession = {
@@ -40,40 +41,35 @@ final class StreamingSession<ResultType: Codable>: NSObject, Identifiable, URLSe
             onProcessingError?(self, StreamingError.unknownContent)
             return
         }
-        let jsonObjects = stringContent
+        let jsonObjects = "\(streamingBuffer)\(stringContent)"
             .components(separatedBy: "data:")
             .filter { $0.isEmpty == false }
             .map { $0.trimmingCharacters(in: .whitespacesAndNewlines) }
+        
+        streamingBuffer = ""
+        
         guard jsonObjects.isEmpty == false, jsonObjects.first != streamingCompletionMarker else {
             return
         }
-        jsonObjects.forEach { jsonContent  in
-            guard !jsonContent.isEmpty else {
-                return
-            }
-            guard jsonContent != streamingCompletionMarker else {
+        jsonObjects.enumerated().forEach { (index, jsonContent)  in
+            guard jsonContent != streamingCompletionMarker && !jsonContent.isEmpty else {
                 return
             }
             guard let jsonData = jsonContent.data(using: .utf8) else {
                 onProcessingError?(self, StreamingError.unknownContent)
                 return
             }
-            
-            var apiError: Error? = nil
+            let decoder = JSONDecoder()
             do {
-                let decoder = JSONDecoder()
                 let object = try decoder.decode(ResultType.self, from: jsonData)
                 onReceiveContent?(self, object)
             } catch {
-                apiError = error
-            }
-            
-            if let apiError = apiError {
-                do {
-                    let decoded = try JSONDecoder().decode(APIErrorResponse.self, from: jsonData)
+                if let decoded = try? decoder.decode(APIErrorResponse.self, from: jsonData) {
                     onProcessingError?(self, decoded)
-                } catch {
-                    onProcessingError?(self, apiError)
+                } else if index == jsonObjects.count - 1 {
+                    streamingBuffer = "data: \(jsonContent)" // Chunk ends in a partial JSON
+                } else {
+                    onProcessingError?(self, error)
                 }
             }
         }
